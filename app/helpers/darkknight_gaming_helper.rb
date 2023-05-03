c2c7bf26ba4445e6a41dc4a307f58ea7b55d2250
Navdeep Singh Shekhawat <navdeep.shekhawat@deeporion.com>
2023-05-03 18:41:53 +0530

    3 may commit- Creating , generating ,  interact with  model and showing articles

---
+++ app/helpers/darkknight_gaming_helper.rb
@@ -0,0 +1,2 @@
+module DarkknightGamingHelper
+end
---
+++ test/controllers/darkknight_gaming_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DarkknightGamingControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
